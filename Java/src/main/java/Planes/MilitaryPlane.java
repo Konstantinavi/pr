a48f269dd0cd7075325e16bdc5e78fe5616ee8e0
Ангелина Константинавичюс <Konstantinavi_AD_25@student.grsu.by>
2026-07-07 14:23:46 +0300

    Update MilitaryPlane.java

--- Java/src/main/java/Planes/MilitaryPlane.java
+++ Java/src/main/java/Planes/MilitaryPlane.java
@@ -1,40 +1,59 @@
-package Planes;
-
-import models.MilitaryType;
+package planes;
 
 import java.util.Objects;
 
-public class MilitaryPlane extends Plane{
+public abstract class Plane {
+    private final String model;
+    private final int maxSpeed;
+    private final int maxFlightDistance;
+    private final int maxLoadCapacity;
 
-    private MilitaryType type;
-
-    public MilitaryPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
+    protected Plane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.maxFlightDistance = maxFlightDistance;
+        this.maxLoadCapacity = maxLoadCapacity;
     }
 
-    public MilitaryType getType() {
-        return type;
+    public String getModel() {
+        return model;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getMaxFlightDistance() {
+        return maxFlightDistance;
+    }
+
+    public int getMaxLoadCapacity() {
+        return maxLoadCapacity;
     }
 
     @Override
     public String toString() {
-        return super.toString().replace("}",
-                ", type=" + type +
-                '}');
+        return "Plane{" +
+                "model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", maxFlightDistance=" + maxFlightDistance +
+                ", maxLoadCapacity=" + maxLoadCapacity +
+                '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof MilitaryPlane)) return false;
-        if (!super.equals(o)) return false;
-        MilitaryPlane that = (MilitaryPlane) o;
-        return type == that.type;
+        if (o == null || getClass() != o.getClass()) return false;
+        Plane plane = (Plane) o;
+        return maxSpeed == plane.maxSpeed &&
+                maxFlightDistance == plane.maxFlightDistance &&
+                maxLoadCapacity == plane.maxLoadCapacity &&
+                Objects.equals(model, plane.model);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), type);
+        return Objects.hash(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
     }
 }
