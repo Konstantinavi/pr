c11394f4b32fd4cd9b389c60738a3009ea2a7a9d
Ангелина Константинавичюс <Konstantinavi_AD_25@student.grsu.by>
2026-07-07 14:25:43 +0300

    Update MyPlane.java

--- Java/src/main/java/Planes/MyPlane.java
+++ Java/src/main/java/Planes/MyPlane.java
@@ -1,9 +1,28 @@
-package Planes;
+package planes;
 
-public class MyPlane  extends Plane {
+public class MyPlane extends Plane {
+    
     public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
     }
 
-    //TODO implement it later
+    @Override
+    public String toString() {
+        return "MyPlane{" +
+                "super=" + super.toString() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
+
